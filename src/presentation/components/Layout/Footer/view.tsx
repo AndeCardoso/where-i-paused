31be451abdefd8fe8f8d7673048025b3fe64175f
AndeCardoso <andersoncardoso.dev@gmail.com>
2025-04-21 10:55:10 -0300

    update: Add spacing treatment to layout component in android version

--- src/presentation/components/Layout/Footer/view.tsx
+++ src/presentation/components/Layout/Footer/view.tsx
@@ -1,8 +1,16 @@
 import React, { PropsWithChildren } from "react";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
+
+const android = Platform.OS === "android";
 
 export const Footer = ({ children }: PropsWithChildren) => {
   return (
-    <View className="h-auto gap-2 pt-4 px-6 border-t-hairline">{children}</View>
+    <View
+      className={`h-auto gap-2 pt-4 px-6 ${
+        android && "pb-4"
+      } border-t-hairline`}
+    >
+      {children}
+    </View>
   );
 };

d53e3be0cff44d6d1ea4e6a45038010810c16cf2
AndeCardoso <andersoncardoso.dev@gmail.com>
2025-04-21 21:51:06 -0300

    update: Change ton of base colors

--- src/presentation/styles/colors/base.ts
+++ src/presentation/styles/colors/base.ts
@@ -2,5 +2,5 @@ export const base = {
   white: "#FFFFFF",
   black: "#000000",
   transparent: "transparent",
-  backgroundBlur: "#FF7B0042",
+  backgroundBlur: "#FF7B3342",
 };
--- src/presentation/styles/colors/light.ts
+++ src/presentation/styles/colors/light.ts
@@ -2,9 +2,9 @@ import { MD3Colors } from "react-native-paper/lib/typescript/types";
 import { base } from "./base";
 
 export const light: MD3Colors | typeof base = {
-  primary: "#FF7700",
+  primary: "#FF7733",
   primaryContainer: "#FFA657",
-  secondary: "#FFD300",
+  secondary: "#FFD333",
   secondaryContainer: "#FFE66F",
   tertiary: "#FFCCA0",
   tertiaryContainer: "#FFB16D",

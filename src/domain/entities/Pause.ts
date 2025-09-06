ebb79a569069742903cb7fa2666f3defaf8c8bda
AndeCardoso <andersoncardoso.dev@gmail.com>
2025-09-06 11:10:40 -0300

    update: Improving pause entity type

--- src/domain/entities/Pause.ts
+++ src/domain/entities/Pause.ts
@@ -1,12 +1,26 @@
-export interface IPauseDomain {
+export enum EContentTypes {
+  MOVIE = "movie",
+  SERIES = "series",
+  MUSIC = "music",
+}
+
+export type TContentTypes = (typeof EContentTypes)[keyof typeof EContentTypes];
+
+interface ISeriesDomain {
+  season: number;
+  episode: number;
+}
+
+interface IBasePauseDomain {
   id: number;
   title: string;
   contentType: TContentTypes;
   totalTime: string;
   paused: string;
-  season?: number;
-  episode?: number;
   favorited?: boolean;
 }
 
-export type TContentTypes = "movie" | "series" | "music";
+export type IPauseDomain<T extends TContentTypes = TContentTypes> =
+  T extends EContentTypes.SERIES
+    ? IBasePauseDomain & ISeriesDomain
+    : IBasePauseDomain & Partial<ISeriesDomain>;
